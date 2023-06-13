0556b61603e942006f363628146b9dfce0a94b32
naveen <sunkaranaveensunkara@gmail.com>
2023-06-13 20:56:34 +0530

    countDigits

---
+++ countDigits.js
@@ -0,0 +1,11 @@
+function countDigits(num){
+    const arr = num.toString().split("")
+    let sum =0
+for(let i=0;i<arr.length;i++){
+   sum+= parseInt(arr[i]) 
+}
+console.log(sum)
+}
+console.log(countDigits(2021))
+
+// console.log(Number("2"))
