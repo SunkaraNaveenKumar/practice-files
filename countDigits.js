578ef0f062e7addcc29099b3b9c4ca672f5fe923
naveen <sunkaranaveensunkara@gmail.com>
2023-06-14 23:16:43 +0530

    latest commit

--- countDigits.js
+++ countDigits.js
@@ -1,11 +1,15 @@
-function countDigits(num){
-    const arr = num.toString().split("")
-    let sum =0
-for(let i=0;i<arr.length;i++){
-   sum+= parseInt(arr[i]) 
-}
-console.log(sum)
-}
-console.log(countDigits(2021))
+// function countDigits(num){
+//     const arr = num.toString().split("")
+//     let sum =0
+// for(let i=0;i<arr.length;i++){
+//    sum+= parseInt(arr[i]) 
+// }
+// console.log(sum)
+// }
+// console.log(countDigits(2021))
 
 // console.log(Number("2"))
+/////////////////////////////
+
+// const obj={"name" : "nav"}
+// console.group(obj.name)
